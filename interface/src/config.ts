b53e5ad9eaf0a0d1b41af070e25c6808305e2bcf
jtardioli <joshua.tardioli@gmail.com>
2023-08-03 22:50:08 -0400

    sace

--- interface/src/config.ts
+++ interface/src/config.ts
@@ -1,24 +1,3 @@
-import { Color } from "./ts/utils";
-
-export const colors: Record<string, Color> = {
-  "0": "#ffffff",
-  "1": "#aaaaaa",
-  "2": "#555555",
-  "3": "#000000",
-  "4": "#ffff54",
-  "5": "#00aa01",
-  "6": "#55ff54",
-  "7": "#ff5555",
-  "8": "#aa0101",
-  "9": "#aa5501",
-  a: "#aa01aa",
-  b: "#ff55ff",
-  c: "#55fffe",
-  d: "#00aaaa",
-  e: "#0000aa",
-  f: "#5555ff",
-};
-
 interface NetworkConfig {
   chainId: string;
   chainName: string;
